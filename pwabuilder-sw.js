d4dbb9ecee26a63cd53b3f2b6622eaf011021007
Marcus Vinicius <marcusvslourenco@fac.pe.senac.br>
2022-10-19 22:48:01 -0300

    service-worder gerado pelo PWABuilder adicionado

---
+++ pwabuilder-sw.js
@@ -0,0 +1,66 @@
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = 'cache'
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = [
+  '/index.html',
+  '/telaCadastro.html',
+  '/telaContatosUteis.html',
+  '/telaPrincipal.html',
+
+  '/Css/contatosUteis.css',
+  '/Css/main.css',
+  '/Css/mapa.css',
+  '/Css/telaLogin-Cadastro.css',
+
+  './Javascript/pontos.js',
+  './Javascript/responsividade.js',
+
+  './dist/leaflet.markercluster-src.js',
+  './dist/leaflet.markercluster-src.js.map',
+  './dist/leaflet.markercluster.js',
+  './dist/leaflet.markercluster.js.map',
+  './dist/MarkerCluster.css',
+  './dist/MarkerCluster.Default.css',
+
+  './manifest.json'
+];
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      const cachedResponse = await caches.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+    
+      const response = await fetch(event.request);
+    
+      if (!response || response.status !== 200 || response.type !== 'basic') {
+        return response;
+      }
+    
+      return response;
+    })());
+  }
+});
