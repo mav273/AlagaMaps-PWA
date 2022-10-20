12baa9a1148697d88f1c9582ba74b06c79bacdc0
Marcus Vinicius <marcusvslourenco@fac.pe.senac.br>
2022-10-20 15:20:33 -0300

    service-worker substituido por pwabuildersw

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,6 +3,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = 'cache'
+const notdel = ['cache']
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = [
@@ -24,10 +25,7 @@ const offlineFallbackPage = [
   './dist/leaflet.markercluster.js',
   './dist/leaflet.markercluster.js.map',
   './dist/MarkerCluster.css',
-  './dist/MarkerCluster.Default.css',
-
-  './manifest.json',
-  './pwabuilder-sw.js'
+  './dist/MarkerCluster.Default.css'
 ];
 
 self.addEventListener("message", (event) => {
@@ -46,80 +44,29 @@ self.addEventListener('install', async (event) => {
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
-self.addEventListener('activate', function(event) {
-  // Delete all caches that aren't named in CURRENT_CACHES.
-  // While there is only one cache in this example, the same logic will handle the case where
-  // there are multiple versioned caches.
-  var expectedCacheNames = Object.keys(offlineFallbackPage).map(function(key) {
-    return offlineFallbackPage[key];
-  });
 
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (expectedCacheNames.indexOf(cacheName) === -1) {
-            // If this cache name isn't present in the array of "expected" cache names, then delete it.
-            console.log('Deleting out of date cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
-
+addEventListener('fetch', function(event) {
   event.respondWith(
-    // caches.match() will look for a cache entry in all of the caches available to the service worker.
-    // It's an alternative to first opening a specific named cache and then matching on that.
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found response in cache:', response);
-
-        return response;
-      }
-
-      console.log('No response found in cache. About to fetch from network...');
-
-      // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
-      // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
-      return fetch(event.request).then(function(response) {
-        console.log('Response from network is:', response);
-
-        return response;
-      }).catch(function(error) {
-        // This catch() will handle exceptions thrown from the fetch() operation.
-        // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
-        // It will return a normal response object that has the appropriate error code set.
-        console.error('Fetching failed:', error);
-
-        throw error;
-      });
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
+        }
+      })
   );
-});
-
-// self.addEventListener('fetch', (event) => {
-//   if (event.request.mode === 'navigate') {
-//     event.respondWith((async () => {
-//       try {
-//         const preloadResp = await event.preloadResponse;
-
-//         if (preloadResp) {
-//           return preloadResp;
-//         }
-
-//         const networkResp = await fetch(event.request);
-//         return networkResp;
-//       } catch (error) {
-
-//         const cache = await caches.open(CACHE);
-//         const cachedResp = await cache.matchAll(offlineFallbackPage);
-//         return cachedResp;
-//       }
-//     })());
-//   }
-// });
+}); 
