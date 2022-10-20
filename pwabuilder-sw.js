364b9264d5625fb72078c2b1332feeac6b72edae
Marcus Vinicius <marcusvslourenco@fac.pe.senac.br>
2022-10-20 15:23:37 -0300

    Erro ao carregar página corrigido

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -52,20 +52,7 @@ addEventListener('fetch', function(event) {
         if (response) {
           return response;     // if valid response is found in cache return it
         } else {
-          return fetch(event.request)     //fetch from internet
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());    //save the response for future
-                  return res;   // return the fetched data
-                })
-            })
-            .catch(function(err) {       // fallback mechanism
-              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                .then(function(cache) {
-                  return cache.match('/offline.html');
-                });
-            });
+          console.log("Deu erro")
         }
       })
   );
