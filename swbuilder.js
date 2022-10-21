b5c9c76196bdfd715ccaa08232b0be99fc633a5d
mav273 <marcusvslourenco@fac.pe.senac.br>
2022-10-20 22:24:30 -0300

    service-worker novo

--- swbuilder.js
+++ swbuilder.js
@@ -1,18 +1,60 @@
-// This is the "Offline copy of pages" service worker
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'index.html';
+var ASSETS = [
+  '/index.html',
+  '/Css/telaLogin-Cadastro.css'
+];
 
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// self.oninstall = function (evt) {
+//   evt.waitUntil(caches.open('offline-cache-name').then(function (cache) {
+//     return cache.addAll(ASSETS);
+//   }))
+// };
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+       
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+       
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
+
+});
